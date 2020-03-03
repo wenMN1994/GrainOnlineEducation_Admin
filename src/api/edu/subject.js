12f3869114079cd2f093fd874618ee130d9672ac
温文星 <18475536452@163.com>
2020-03-03 11:17:36 +0800

    前端课程分类列表页面删除新增接口联调

--- src/api/edu/subject.js
+++ src/api/edu/subject.js
@@ -9,5 +9,26 @@ export default {
       url: `${api_name}/tree`,
       method: 'get'
     })
+  },
+  removeById(id) {
+    return request({
+      url: `${api_name}/${id}`,
+      method: 'delete'
+    })
+  },
+  saveLevelOne(subject) {
+    return request({
+      url: `${api_name}/saveLevelOne`,
+      method: 'post',
+      data: subject
+    })
+  },
+  saveLevelTwo(subject) {
+    return request({
+      url: `${api_name}/saveLevelTwo`,
+      method: 'post',
+      data: subject
+    })
   }
+
 }
--- src/views/subject/list.vue
+++ src/views/subject/list.vue
@@ -2,6 +2,8 @@
   <div class="app-container">
     <el-input v-model="filterText" placeholder="Filter keyword" style="margin-bottom:30px;" />
 
+    <el-button type="text" @click="dialogFormVisible = true">添加一级分类</el-button>
+
     <el-tree
       ref="subjectTree"
       :data="subjectList"
@@ -19,15 +21,26 @@
             v-if="node.level == 1"
             type="text"
             size="mini"
-            @click="() => append(data)">添加</el-button>
+            @click="() => {dialogFormVisible = true; subject.parentId = data.id}">添加</el-button>
           <el-button
-            v-if="node.level == 2"
             type="text"
             size="mini"
             @click="() => remove(node, data)">删除</el-button>
         </span>
       </span>
     </el-tree>
+
+    <el-dialog :visible.sync="dialogFormVisible" title="添加分类">
+      <el-form :model="subject" label-width="120px">
+        <el-form-item label="分类标题">
+          <el-input v-model="subject.title"/>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="appen()">确 定</el-button>
+      </div>
+    </el-dialog>
 
   </div>
 </template>
@@ -43,6 +56,11 @@ export default {
       defaultProps: {
         children: 'children',
         label: 'title'
+      },
+      dialogFormVisible: false,
+      subject: {
+        title: '',
+        parentId: 0
       }
     }
   },
@@ -67,6 +85,79 @@ export default {
     filterNode(value, data) {
       if (!value) return true
       return data.title.toLowerCase().indexOf(value.toLowerCase()) !== -1
+    },
+    remove(node, data) {
+      console.log(node)
+      console.log(data)
+
+      this.$confirm('此操作将永久删除该记录, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        return subject.removeById(data.id)
+      }).then(() => {
+        // this.fetchNodeList()// 刷新列表
+        this.$refs.subjectTree.remove(node) // 删除节点（效率高）
+        this.$message({
+          type: 'success',
+          message: '删除成功!'
+        })
+      }).catch((response) => { // 失败
+        if (response === 'cancel') {
+          this.$message({
+            type: 'info',
+            message: '已取消删除'
+          })
+        } else {
+          this.$message({
+            type: 'error',
+            message: '删除失败'
+          })
+        }
+      })
+    },
+    appen() {
+      if (this.subject.parentId === 0) {
+        this.appendLevelOne() // 一级分类添加
+      } else {
+        this.appendLevelTwo() // 二级分类添加
+      }
+    },
+    appendLevelOne() {
+      subject.saveLevelOne(this.subject).then(response => {
+        this.$message({
+          type: 'success',
+          message: '添加一级分类成功!'
+        })
+        this.dialogFormVisible = false// 如果保存成功则关闭对话框
+        this.fetchNodeList()// 刷新列表
+        this.subject.title = ''
+      }).catch((response) => {
+        // console.log(response)
+        this.$message({
+          type: 'error',
+          message: '添加一级分类失败'
+        })
+      })
+    },
+    appendLevelTwo() {
+      subject.saveLevelTwo(this.subject).then(response => {
+        this.$message({
+          type: 'success',
+          message: '保存成功!'
+        })
+        this.dialogFormVisible = false// 如果保存成功则关闭对话框
+        this.fetchNodeList()// 刷新列表
+        this.subject.title = ''// 重置类别标题
+        this.subject.parentId = '' // 重置表单parentId
+      }).catch((response) => {
+        // console.log(response)
+        this.$message({
+          type: 'error',
+          message: response.data.message
+        })
+      })
     }
   }
 }
